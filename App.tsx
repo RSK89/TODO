203e240ed3ea621ffd652478cf6edd245792f239
RSK89 <pagehandler2020@gmail.com>
2022-07-19 02:29:05 -0700

    1 file modified

--- App.tsx
+++ App.tsx
@@ -3,21 +3,25 @@ import './style.css';
 import React, { useState } from 'react';
 
 export default function App() {
-  let [data, SetData] = useState(null);
-  let [List, SetList] = useState(['']);
+  let [Task, SetData] = useState(null);
+  let [List, SetList] = useState([]);
   const UpdateData = (e) => {
     SetData(e.target.value);
   };
-  const add = () => {
-    SetList([...List, data]);
+  const addTask = () => {
+    SetList([...List, Task]);
   };
   return (
     <>
       <div>
         <input type="text" onChange={UpdateData} />
-        <button onClick={add}>ADD</button>
+        <button onClick={addTask}>ADD</button>
       </div>
-      <div>{List}</div>
+      <div>
+        Total Tasks TODO is :{List.length}
+        <br />
+        {List}
+      </div>
       <div>{}</div>
       <div>{}</div>
     </>
